6f578267f1decfd2c2ac38ab7bd824bae11c3c34
reymaa19 <rcmaala@gmail.com>
2023-04-06 02:27:10 -0500

    Fix save_data not inserting properly.

--- db_operations.py
+++ db_operations.py
@@ -10,8 +10,6 @@ class DBOperations:
         """Initializes the database path name."""
         self.db_name = path
 
-    # *** Create tables, Insert data, Query data ***
-
     def fetch_data(self) -> list: 
         """
         Fetches requested data from the database for plotting.
@@ -24,36 +22,25 @@ class DBOperations:
             return fetch_weather
         except Exception as e:
             print("*** Error fetching data:", e)
-
-
-
-
-
     
     def save_data(self, data_source: dict):
         """
         Saves new data to the database.
         """
-        new_weather_data = [] # CREATE NEW LIST
-        for sample_date, data in data_source.items(): # FOR EVERY KEY AND VALUE IN DICTIONARY
-            new_sample = [sample_date] # sample = ['2023-05-05']
-            for stat, value in data.items(): # FOR EVERY MEAN, MIN, OR AVERAGE AND ITS VALUE
-                new_sample.append(value) # ADD IT TO THE NEW ROW `['2023-05-05', '1.3', '0.3', '1']`
-            new_weather_data.append(tuple(new_sample)) # ADD NEW SAMPLE TO LIST OF TUPLES `[('2023-05-05', '1.3', '0.3', '1'), (...), (...)]`
+        new_weather_data = []
+        for sample_date, data in data_source.items():
+            new_sample = [sample_date]
+            new_sample.append('Winnipeg, MB')
+            for stat, value in data.items():
+                new_sample.append(value)
+            new_weather_data.append(tuple(new_sample))
         try:
             with DBCM(self.db_name) as cursor:
-                sql_save_data = """INSERT OR IGNORE INTO WeatherData (sample_date, max_temp, min_temp, avg_temp) VALUES (?,?,?,?);"""
-                # SOMETHING WRONG HERE
+                sql_save_data = """INSERT INTO WeatherData (sample_date, location, max_temp, min_temp, avg_temp) VALUES (?,?,?,?,?);"""
                 for date_data in new_weather_data:
                     cursor.execute(sql_save_data, date_data)
-                # SUPPOSED TO COMMIT AFTER EXITING `with` STATEMENT
-                # !!! LOCATION IS NOT BEING RECORDED !!!
         except Exception as e:
             print("*** Error inserting data.", e)
-
-
-
-
 
     def initialize_db(self):
         """
@@ -61,13 +48,14 @@ class DBOperations:
         """
         try: 
             with DBCM(self.db_name) as cursor:
-                SQL_INITIALIZE_DB = """CREATE TABLE IF NOT EXISTS WeatherData
-                (id integer primary key autoincrement not null,
-                    sample_date text not null UNIQUE,
-                    location text not null UNIQUE,
-                    min_temp real not null,
-                    max_temp real not null,
-                    avg_temp real not null); """
+                SQL_INITIALIZE_DB = """CREATE TABLE IF NOT EXISTS WeatherData 
+    (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    sample_date TEXT NOT NULL, 
+    location TEXT NOT NULL,
+    min_temp REAL NOT NULL,
+    max_temp REAL NOT NULL,
+    avg_temp REAL NOT NULL,
+    UNIQUE(sample_date, location));"""
                 cursor.execute(SQL_INITIALIZE_DB)
         except Exception as e:
             print("*** Error creating table:", e)
@@ -81,6 +69,7 @@ if __name__ == '__main__':
 
     my_scraper = WeatherScraper()
     my_scraper.scrape_month_weather_temp_data(2023, 3)
-    print(my_scraper.weather)
-    mydb.save_data(my_scraper.weather) # SAVING NO DATA (save_data not executing properly)
+
+    mydb.save_data(my_scraper.weather)
+
     print(mydb.fetch_data())
