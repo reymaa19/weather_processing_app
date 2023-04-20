25adda6d56db1820e46972cd2a80cf771d711821
John Russel Tabelina <jtabelina@aceproject.space>
2023-04-20 14:53:59 -0500

    updated plot_operations to pass PEP8 standard

--- plot_operations.py
+++ plot_operations.py
@@ -1,40 +1,38 @@
-"""This module is to create different plots """
-import matplotlib.pyplot as plt
 import calendar
+import matplotlib.pyplot as plt
 from db_operations import DBOperations
 
 
-class PlotOperations():
+class PlotOperations:
     def __init__(self, start_year=1996, end_year=2023):
-        """Set up default years"""
+        """Set up default years."""
         self.start_year = start_year
         self.end_year = end_year
-        self.weather_data = dict()
+        self.weather_data = {}
         self.month = 0
         self.my_db = DBOperations('weather.sqlite')
 
     def create_weather_data(self):
-        """Read data from the database"""
+        """Read data from the database."""
         self.weather_data = self.my_db.fetch_data_years(self.start_year, self.end_year)
-        
+
     def create_plot(self, data):
-        """This method creates a plot with a range of years"""
+        """Create a plot with a range of years."""
         fig, ax = plt.subplots()
         ax.boxplot(data.values())
         ax.set_xticklabels(data.keys())
         ax.set(xlabel='Month', ylabel='Temperature (Celsius)',
-            title=f"Monthly Temperature Distribution for: {self.start_year} to {self.end_year}")
+               title=f"Monthly Temperature Distribution for: {self.start_year} to {self.end_year}")
         plt.show()
 
-
     def create_day_plot(self, year, month):
-        """This method creates a monthly plot"""
+        """Create a monthly plot."""
         monthly_list = self.my_db.fetch_data_month(year, month)
 
         plt.plot(monthly_list)
         plt.xlabel('Day')
         plt.ylabel('Temperature (Celsius)')
-        plt.title('Daily Temperature Distribution for : ' +
+        plt.title('Daily Temperature Distribution for: ' +
                   str(year) + ' ' + calendar.month_name[month])
         plt.show()
 
